2e8a4458932a108de81958aa9433fd6cbc4b5f69
Motjiang <motjiangbrilliant@gmail.com>
2024-11-30 22:41:12 +0200

    Added Repositories

---
+++ Todo_API/Repositories/Implementation/TodoRepository.cs
@@ -0,0 +1,33 @@
+﻿using Todo_API.Models.Domain;
+using Todo_API.Repositories.Interface;
+
+namespace Todo_API.Repositories.Implementation
+{
+    public class TodoRepository : ITodoRepository
+    {
+        public Task<Todo> CreateTodoAsync(Todo todo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Todo> DeleteTodoAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Todo> GetTodoById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Todo>> GetTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Todo> UpdateTodoAsync(Todo todo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Todo_API/Repositories/Interface/ITodoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Todo_API.Models.Domain;
+
+namespace Todo_API.Repositories.Interface
+{
+    public interface ITodoRepository
+    {
+
+        Task<IEnumerable<Todo>> GetTodos();
+        Task<Todo> GetTodoById(Guid id);
+        Task<Todo> CreateTodoAsync(Todo todo);
+        Task<Todo> UpdateTodoAsync(Todo todo);
+        Task<Todo> DeleteTodoAsync(Guid id);
+    }
+}
