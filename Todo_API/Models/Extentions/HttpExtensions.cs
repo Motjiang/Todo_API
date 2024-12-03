b9353869a9d7d71938687abbc5a116260e89a17f
Motjiang <motjiangbrilliant@gmail.com>
2024-12-03 06:25:03 +0200

    Added a class for PaginationHeader and HttpExtensions

---
+++ Todo_API/Models/Extentions/HttpExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using Todo_API.Models.Helper;
+
+namespace Todo_API.Models.Extentions
+{
+    public static class HttpExtensions
+    {
+
+        public static void AppPaginationHeader(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+
+        }
+    }
+}
